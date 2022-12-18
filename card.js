83cdbc235646823d228908bb989626eea746d937
Annie Lu <Anniel1210@gmail.com>
2022-12-18 08:22:24 -0500

    blackjack

---
+++ card.js
@@ -0,0 +1,29 @@
+// 1. Create two variables, firstCard and secondCard.
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// let sumEl = document.querySelector("#sum-el")
+
+
+function startGame() {
+    sumEL.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = " Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "BlackJack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
