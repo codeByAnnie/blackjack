6e47f4e8cd4bc181bc0ef49e2cb79d1156b03ad8
Annie Lu <Anniel1210@gmail.com>
2022-12-18 12:31:02 -0500

    update function under card.js

--- card.js
+++ card.js
@@ -12,9 +12,11 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 // let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEL.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = " Do you want to draw a new card?"
