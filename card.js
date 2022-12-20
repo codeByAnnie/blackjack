311a51e5927042ae36e9f3eacd4061aac318b7a9
Annie Lu <Anniel1210@gmail.com>
2022-12-20 18:07:35 -0500

    blackjack game

--- card.js
+++ card.js
@@ -2,22 +2,52 @@
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let cards = [ ] //array - ordered list of items
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 // let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "Per",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else { 
+        return randomNumber
+    }   
+}
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEL.textContent = "Sum: " + sum
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum
+
     if (sum <= 20) {
         message = " Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -29,3 +59,14 @@ function startGame() {
     }
     messageEl.textContent = message
 }
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+   
+}
+
